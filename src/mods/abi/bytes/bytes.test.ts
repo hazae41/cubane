318274d056f9d1d0965845ac278e641526195c6a
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-07-29 12:21:38 +0200

    clean

--- src/mods/abi/bytes/bytes.test.ts
+++ src/mods/abi/bytes/bytes.test.ts
@@ -1,17 +1,17 @@
 import { Readable, Writable } from "@hazae41/binary";
 import { Bytes } from "@hazae41/bytes";
 import { assert, test } from "@hazae41/phobos";
-import { Abi } from "index.js";
+import { DynamicBytes } from "./bytes.js";
 
 test("bytes", async ({ message, test }) => {
   const value = Bytes.tryRandom(123).unwrap()
-  const abi = Abi.DynamicBytes.new(value)
+  const abi = DynamicBytes.new(value)
   const bytes = Writable.tryWriteToBytes(abi).unwrap()
 
   console.log(message, value)
   console.log(message, Bytes.toHex(bytes))
 
-  const abi2 = Readable.tryReadFromBytes(Abi.DynamicBytes, bytes).unwrap()
+  const abi2 = Readable.tryReadFromBytes(DynamicBytes, bytes).unwrap()
   const value2 = abi2.value
 
   console.log(message, value2)
--- src/mods/abi/tuple/tuple.test.ts
+++ src/mods/abi/tuple/tuple.test.ts
@@ -1,14 +1,14 @@
 import { Bytes } from "@hazae41/bytes";
 import { test } from "@hazae41/phobos";
-import { Abi } from "index.js";
 import { tryDecode, tryEncode } from "../abi.js";
 import { Bool } from "../bool/bool.js";
+import { DynamicBytes } from "../bytes/bytes.js";
 import { String } from "../string/string.js";
 import { Uint256 } from "../uint/uint.js";
 
 test("abi", async ({ message, test }) => {
-  const bytes = tryEncode(Bool.new(true), String.new("hello"), Uint256.new(123n), Abi.DynamicBytes.new(Bytes.random(123))).unwrap()
+  const bytes = tryEncode(Bool.new(true), String.new("hello"), Uint256.new(123n), DynamicBytes.new(Bytes.random(123))).unwrap()
   console.log(message, Bytes.toHex(bytes))
-  const abi = tryDecode(bytes, Bool, String, Uint256, Abi.DynamicBytes).unwrap()
+  const abi = tryDecode(bytes, Bool, String, Uint256, DynamicBytes).unwrap()
   console.log(message, abi)
 })
