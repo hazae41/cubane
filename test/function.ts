1e6487b7c84cae45694f122156b7538daea7fa5f
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-10-25 19:28:33 +0200

    function

--- test/function.ts
+++ test/function.ts
@@ -2,4 +2,4 @@ import "@hazae41/symbol-dispose-polyfill"
 import { Cubane } from "../src/index.js"
 import "./algorithms.js"
 
-export const f = Cubane.Abi.createFunctionSignature("f",Cubane.Abi.createFunctionSelectorAndArguments(Cubane.Abi.FunctionSelector.from([196,183,30,19]),Cubane.Abi.createDynamicTuple(Cubane.Abi.StaticBool,Cubane.Abi.createStaticBigUint(32),Cubane.Abi.DynamicString)))
+export const f = Cubane.Abi.createFunctionSignature("f",Cubane.Abi.createFunctionSelectorAndArguments(Cubane.Abi.FunctionSelector.from([196,183,30,19]),Cubane.Abi.createDynamicTuple(Cubane.Abi.StaticBool,Cubane.Abi.Uint256,Cubane.Abi.DynamicString)))
