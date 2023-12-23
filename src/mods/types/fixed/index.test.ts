a1fa3c515cc9b7f241c42a1437c1827fd1885541
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-12-23 15:15:14 +0100

    test

--- src/mods/types/fixed/index.test.ts
+++ src/mods/types/fixed/index.test.ts
@@ -8,5 +8,19 @@ test("fixed", async () => {
   assert(Fixed.fromString(".1", 2).value === 10n)
   assert(Fixed.fromString(".", 2).value === 0n)
   assert(Fixed.fromString("0", 2).value === 0n)
-  console.log(Fixed.fromString("123.456", 18).round().toString())
+
+  assert(Fixed.fromString("0.1", 2).toString() === "0.1")
+  assert(Fixed.fromString(".1", 2).toString() === "0.1")
+
+  assert(Fixed.fromString("1", 2).toString() === "1")
+  assert(Fixed.fromString("1.0", 2).toString() === "1")
+  assert(Fixed.fromString("1.00", 2).toString() === "1")
+
+  assert(Fixed.fromString("123.456", 18).round().toString() === "123")
+  assert(Fixed.fromString("123.456", 18).floor().toString() === "123")
+  assert(Fixed.fromString("123.456", 18).ceil().toString() === "124")
+
+  assert(Fixed.fromString("123.654", 18).round().toString() === "124")
+  assert(Fixed.fromString("123.654", 18).floor().toString() === "123")
+  assert(Fixed.fromString("123.654", 18).ceil().toString() === "124")
 })
