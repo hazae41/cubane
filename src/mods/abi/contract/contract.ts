c958ec1fa7eba04613cb25de1346ac289e06557d
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-01-21 23:19:10 +0100

    clean

--- src/mods/abi/contract/contract.ts
+++ src/mods/abi/contract/contract.ts
@@ -1,38 +1,38 @@
-import { Abi, ZeroHexString } from "index.js";
-import { AbiFunction, AbiInstance, Address, FunctionSelectorAndArguments } from "../index.js";
+// import { Abi, ZeroHexString } from "index.js";
+// import { AbiFunction, AbiInstance, Address, FunctionSelectorAndArguments } from "../index.js";
 
-export class AbiContract<M extends Record<string, AbiFunction<any, any>>> {
+// export class AbiContract<M extends Record<string, AbiFunction<any, any>>> {
 
-  constructor(
-    readonly funcs: M
-  ) { }
+//   constructor(
+//     readonly funcs: M
+//   ) { }
 
-}
+// }
 
-export interface CallData {
-  readonly to: Address
-  readonly data: ZeroHexString
-}
+// export interface CallData {
+//   readonly to: Address
+//   readonly data: ZeroHexString
+// }
 
-export interface Caller {
-  call(data: CallData): Promise<ZeroHexString>
-}
+// export interface Caller {
+//   call(data: CallData): Promise<ZeroHexString>
+// }
 
-export class Contract<M extends Record<string, AbiFunction<any, any>>> {
+// export class Contract<M extends Record<string, AbiFunction<any, any>>> {
 
-  constructor(
-    readonly address: Address,
-    readonly caller: Caller,
-    readonly abi: AbiContract<M>
-  ) { }
+//   constructor(
+//     readonly address: Address,
+//     readonly caller: Caller,
+//     readonly abi: AbiContract<M>
+//   ) { }
 
-  async call(key: keyof M, args: AbiFunction.Args<M[keyof M]>): Promise<AbiInstance<AbiFunction.Rets<M[keyof M]>>> {
-    const func = this.abi.funcs[key]
+//   async call(key: keyof M, args: AbiFunction.Args<M[keyof M]>): Promise<AbiInstance<AbiFunction.Rets<M[keyof M]>>> {
+//     const func = this.abi.funcs[key]
 
-    const input = Abi.encodeOrThrow(FunctionSelectorAndArguments.create(func.func, func.args).create(args))
-    const output = await this.caller.call({ to: this.address, data: input })
+//     const input = Abi.encodeOrThrow(FunctionSelectorAndArguments.create(func.func, func.args).create(args))
+//     const output = await this.caller.call({ to: this.address, data: input })
 
-    return Abi.decodeOrThrow(func.rets as AbiFunction.Rets<M[keyof M]>, output)
-  }
+//     return Abi.decodeOrThrow(func.rets as AbiFunction.Rets<M[keyof M]>, output)
+//   }
 
-}
+// }
