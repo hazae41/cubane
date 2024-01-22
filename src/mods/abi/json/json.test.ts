f6f785bef01366587c83eaac29b933b970b968f9
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-01-22 16:11:14 +0100

    fast

--- src/mods/abi/json/json.test.ts
+++ src/mods/abi/json/json.test.ts
@@ -2,7 +2,7 @@ import { Base16 } from "@hazae41/base16";
 import { Keccak256 } from "@hazae41/keccak256";
 
 import { test } from "@hazae41/phobos";
-import { AbiBool, AbiUint256 } from "../index.js";
+import { AbiBool, AbiUint256, BytesAbiUint256 } from "../index.js";
 import { AbiNamed, AbiStruct } from "./json.js";
 
 Base16.set(await Base16.fromBufferOrAlocer())
@@ -14,7 +14,7 @@ test("struct", async () => {
   const myStruct = MyStruct.from({ b: true, a: 123n })
   const myStruct2 = MyStruct.from([123n, true])
 
-  const myStruct3 = MyStruct.create({ a: AbiUint256.from(123), b: AbiBool.from(true) })
+  const myStruct3 = MyStruct.create({ a: BytesAbiUint256.from(new Uint8Array([123])), b: AbiBool.from(true) })
 
   console.log(myStruct.encodeOrThrow())
   console.log(myStruct2.encodeOrThrow())
