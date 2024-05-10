37962ad4dc9f964707dcc5f8cf1cfd9d110f0732
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-05-10 12:20:08 +0700

    sign

--- src/mods/types/signer/index.ts
+++ src/mods/types/signer/index.ts
@@ -1,3 +1,5 @@
+import { Bytes } from "@hazae41/bytes";
+import { Keccak256 } from "@hazae41/keccak256";
 import { Secp256k1 } from "@hazae41/secp256k1";
 import { WasmSignature } from "../signature/index.js";
 import { BytesString } from "../string/index.js";
@@ -12,8 +14,18 @@ export class WasmPrivateKey {
     return new WasmPublicKey(this.value.getPublicKeyOrThrow())
   }
 
-  signPersonalMessageOrThrow(message: BytesString.From) {
+  signUnsafeMessageOrThrow(message: BytesString.From) {
     return new WasmSignature(this.value.signOrThrow(BytesString.fromOrThrow(message)))
+  }
+
+  signPersonalMessageOrThrow(message: BytesString.From) {
+    const bytesm = BytesString.fromOrThrow(message)
+    const prefix = Bytes.fromUtf8("\x19Ethereum Signed Message:\n" + bytesm.length.toString())
+
+    const concat = Bytes.concat([prefix, bytesm])
+    using hash = Keccak256.get().hashOrThrow(concat)
+
+    return new WasmSignature(this.value.signOrThrow(hash))
   }
 
 }
