3d907caeb3093aad18b4b72723eff356331d50b2
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-05-02 23:15:43 +0700

    d

--- src/mods/types/secp256k1/signing/index.ts
+++ src/mods/types/secp256k1/signing/index.ts
@@ -7,7 +7,8 @@ import { Secp256k1 } from "@hazae41/secp256k1";
 import { Copiable } from "libs/copiable/index.js";
 import { Address } from "mods/types/address/index.js";
 import { BytesAsInteger, BytesAsUtf8, ZeroHexAsInteger } from "mods/types/formats/index.js";
-import { RsvBytesSignature } from "../signature/index.js";
+import { ExtSignature, RsvBytesSignature } from "../signature/index.js";
+import { ExtVerifyingKey } from "../verifying/index.js";
 
 export type SigningKey =
   | ZeroHexSigningKey
@@ -21,72 +22,36 @@ export namespace SigningKey {
     | BytesSigningKey.From
     | ExtSigningKey.From
 
-  export function randomOrThrow(): ExtSigningKey {
-    return Secp256k1.get().getOrThrow().SigningKey.randomOrThrow()
+  export function randomOrThrow() {
+    return ExtSigningKey.randomOrThrow()
   }
 
   export function getVerifyingKeyOrThrow(signingKey: SigningKey.From) {
-    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
-    const verifyingKeyExt = signingKeyExtBox.get().getVerifyingKeyOrThrow()
-
-    return verifyingKeyExt
+    return ExtSigningKey.getVerifyingKeyOrThrow(signingKey)
   }
 
   export function getUncheckedAddressOrThrow(signingKey: SigningKey.From) {
-    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
-
-    using verifyingKeyExt = signingKeyExtBox.get().getVerifyingKeyOrThrow()
-    using verifyingKeyMemoryExt = verifyingKeyExt.exportUncompressedOrThrow()
-
-    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(verifyingKeyMemoryExt.bytes.subarray(1))
-    const rawLowerCase = Base16.get().getOrThrow().encodeOrThrow(hashMemoryExt)
-
-    return `0x${rawLowerCase.slice(-40)}` as ZeroHexString<20>
+    return ExtSigningKey.getUncheckedAddressOrThrow(signingKey)
   }
 
   export function getAddressOrThrow(signingKey: SigningKey.From) {
-    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
+    return ExtSigningKey.getAddressOrThrow(signingKey)
+  }
 
-    using verifyingKeyExt = signingKeyExtBox.get().getVerifyingKeyOrThrow()
-    using verifyingKeyMemoryExt = verifyingKeyExt.exportUncompressedOrThrow()
-
-    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(verifyingKeyMemoryExt.bytes.subarray(1))
-    const rawLowerCase = Base16.get().getOrThrow().encodeOrThrow(hashMemoryExt)
-
-    return Address.fromRawHexOrThrow(rawLowerCase.slice(-40) as RawHexString)
+  export function signUnprefixedMessageNoOffsetOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): ExtSignature {
+    return ExtSigningKey.signUnprefixedMessageNoOffsetOrThrow(signingKey, message)
   }
 
   export function signUnprefixedMessageOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): RsvBytesSignature {
-    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
-    const messageBytes = BytesAsUtf8.fromOrThrow(message)
+    return ExtSigningKey.signUnprefixedMessageOrThrow(signingKey, message)
+  }
 
-    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(messageBytes)
-    using signatureExt = signingKeyExtBox.get().signOrThrow(hashMemoryExt)
-
-    const signatureRsvBytes = RsvBytesSignature.fromExtOrThrow(signatureExt)
-
-    const { r, s } = signatureRsvBytes
-    const v = signatureRsvBytes.v + 27
-
-    return { r, s, v }
+  export function signMessageNoOffsetOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): ExtSignature {
+    return ExtSigningKey.signMessageNoOffsetOrThrow(signingKey, message)
   }
 
   export function signMessageOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): RsvBytesSignature {
-    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
-    const messageBytes = BytesAsUtf8.fromOrThrow(message)
-
-    const prefixBytes = Bytes.fromUtf8("\x19Ethereum Signed Message:\n" + messageBytes.length.toString())
-    const concatBytes = Bytes.concat([prefixBytes, messageBytes])
-
-    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(concatBytes)
-    using signatureExt = signingKeyExtBox.get().signOrThrow(hashMemoryExt)
-
-    const signatureRsvBytes = RsvBytesSignature.fromExtOrThrow(signatureExt)
-
-    const { r, s } = signatureRsvBytes
-    const v = signatureRsvBytes.v + 27
-
-    return { r, s, v }
+    return ExtSigningKey.signMessageOrThrow(signingKey, message)
   }
 
 }
@@ -143,6 +108,10 @@ export type ExtSigningKey = Secp256k1.SigningKey
 
 export namespace ExtSigningKey {
 
+  export function randomOrThrow(): ExtSigningKey {
+    return Secp256k1.get().getOrThrow().SigningKey.randomOrThrow()
+  }
+
   export type From = Secp256k1.SigningKey
 
   export function fromOrThrow(from: SigningKey.From): Box<ExtSigningKey> {
@@ -157,6 +126,74 @@ export namespace ExtSigningKey {
 
   export function fromOtherOrThrow(from: BytesAsInteger.From): Box<ExtSigningKey> {
     return new Box(Secp256k1.get().getOrThrow().SigningKey.importOrThrow(BytesAsInteger.Length.fromOrThrow(from, 32)))
+  }
+
+  export function getVerifyingKeyOrThrow(signingKey: SigningKey.From): ExtVerifyingKey {
+    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
+    return signingKeyExtBox.get().getVerifyingKeyOrThrow()
+  }
+
+  export function getUncheckedAddressOrThrow(signingKey: SigningKey.From): ZeroHexString<20> {
+    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
+
+    using verifyingKeyExt = signingKeyExtBox.get().getVerifyingKeyOrThrow()
+    using verifyingKeyMemoryExt = verifyingKeyExt.exportUncompressedOrThrow()
+
+    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(verifyingKeyMemoryExt.bytes.subarray(1))
+    const rawLowerCase = Base16.get().getOrThrow().encodeOrThrow(hashMemoryExt)
+
+    return `0x${rawLowerCase.slice(-40)}` as ZeroHexString<20>
+  }
+
+  export function getAddressOrThrow(signingKey: SigningKey.From): Address {
+    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
+
+    using verifyingKeyExt = signingKeyExtBox.get().getVerifyingKeyOrThrow()
+    using verifyingKeyMemoryExt = verifyingKeyExt.exportUncompressedOrThrow()
+
+    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(verifyingKeyMemoryExt.bytes.subarray(1))
+    const rawLowerCase = Base16.get().getOrThrow().encodeOrThrow(hashMemoryExt)
+
+    return Address.fromRawHexOrThrow(rawLowerCase.slice(-40) as RawHexString)
+  }
+
+  export function signUnprefixedMessageNoOffsetOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): ExtSignature {
+    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
+    const messageBytes = BytesAsUtf8.fromOrThrow(message)
+
+    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(messageBytes)
+    return signingKeyExtBox.get().signOrThrow(hashMemoryExt)
+  }
+
+  export function signUnprefixedMessageOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): RsvBytesSignature {
+    using signatureExt = signUnprefixedMessageNoOffsetOrThrow(signingKey, message)
+    const signatureRsvBytes = RsvBytesSignature.fromExtOrThrow(signatureExt)
+
+    const { r, s } = signatureRsvBytes
+    const v = signatureRsvBytes.v + 27
+
+    return { r, s, v }
+  }
+
+  export function signMessageNoOffsetOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): ExtSignature {
+    using signingKeyExtBox = ExtSigningKey.fromOrThrow(signingKey)
+    const messageBytes = BytesAsUtf8.fromOrThrow(message)
+
+    const prefixBytes = Bytes.fromUtf8("\x19Ethereum Signed Message:\n" + messageBytes.length.toString())
+    const concatBytes = Bytes.concat([prefixBytes, messageBytes])
+
+    using hashMemoryExt = Keccak256.get().getOrThrow().hashOrThrow(concatBytes)
+    return signingKeyExtBox.get().signOrThrow(hashMemoryExt)
+  }
+
+  export function signMessageOrThrow(signingKey: SigningKey.From, message: BytesAsUtf8.From): RsvBytesSignature {
+    using signatureExt = signMessageNoOffsetOrThrow(signingKey, message)
+    const signatureRsvBytes = RsvBytesSignature.fromExtOrThrow(signatureExt)
+
+    const { r, s } = signatureRsvBytes
+    const v = signatureRsvBytes.v + 27
+
+    return { r, s, v }
   }
 
 }
